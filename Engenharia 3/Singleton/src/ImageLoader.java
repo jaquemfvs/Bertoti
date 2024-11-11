a0432b6ee2fb278949f15392f96722772a26d015
Jaqueline Maria França Veloso da Silva <118978492+jaquemfvs@users.noreply.github.com>
2024-11-11 08:55:56 -0300

    Fix

--- Engenharia 3/Singleton/src/ImageLoader.java
+++ Engenharia 3/Singleton/src/ImageLoader.java
@@ -1,18 +1,21 @@
-package src;
 public class ImageLoader {
-    
     private static ImageLoader instance;
-      private ImageLoader() {}
-      public static ImageLoader getInstance()
-      {
-          if(instance == null)
-              instance = new ImageLoader();
-  
-          return instance;
-      }
-      public FakeImage loadImage(String color)
-      {
-          return new FakeImage(color);
-  
-      }
-  }
+
+    private ImageLoader() {}
+
+    public static ImageLoader getInstance() {
+        if (instance == null) {
+            instance = new ImageLoader();
+        }
+        return instance;
+    }
+
+    public FakeImage loadImage(String color) {
+        return new FakeImage(color);
+    }
+
+    @Override
+    public String toString() {
+        return "ImageLoader Instance: " + this.hashCode();
+    }
+}
--- Engenharia 3/Singleton/src/Main.java
+++ Engenharia 3/Singleton/src/Main.java
@@ -1,15 +1,13 @@
-package src;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         FakeImage newImage = ImageLoader.getInstance().loadImage("azul");
         ImageLoader image3 = ImageLoader.getInstance();
         System.out.println("Carregando uma nova imagem " + newImage.getColor());
-        System.out.println(image3);
+        System.out.println(image3); 
 
         FakeImage newImage1 = ImageLoader.getInstance().loadImage("rosa");
         ImageLoader image2 = ImageLoader.getInstance();
         System.out.println("Carregando uma nova imagem " + newImage1.getColor());
-        System.out.println(image2);
+        System.out.println(image2); 
     }
 }
